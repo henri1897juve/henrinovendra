f44397dee671b6782ee9324f539cdfda64105f56
unknown <henrijvers@gmail.com>
2020-11-26 12:01:19 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="font/css/all.min.css">
+    <title > SDN BENDUNGAN 1</title>
+  </head>
+  <body>
+    
+    <!-- navbar  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#"><span class="fas fa-school fa-sm"></span>SEKOLAH KITA </a>
+          <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav ml-auto">
+                <a class="nav-link" href="#">CARI SEKOLAH</a>
+                <a class="nav-link" href="#">BANTUAN</a>
+                <a class="nav-link" href="#"><i class="fas fa-user"></i>  LOGIN</a>
+              </div>
+          </div>   
+      </div>
+    </nav>
+
+    <!-- end navbar -->
+    <div class="container content">
+      <div class="row">
+        <div class="col-xs-12 col-md-12"> 
+            <header><h4>SD NEGERI BENDUNGAN 1</h4></header>
+            <div class="small-teks">
+             <i class="fas fa-road"> </i>
+             <span>Jl. Raya Bendungan No 18, Bendungan, Kec. Kudu, Kab. Jombang Prov. Jawa Timur </span>
+             </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-md-8">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="img/5.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="img/6.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="..." class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+        </div>
+      <!--end carousel  -->
+        <div class="col-xs-12 col-md-4">
+          <div class="row">
+            <div class="col-xs-12 col-md-12">
+              <ul class="list-group">
+                <li class="list-group-item list-group-item-action active" > <i class="fas fa-info-circle"></i>
+                  Detail Sekolah</li>
+                <li class="list-group-item list-group-item-info "><i class="fas fa-font"></i> Akreditasi B</li>
+                <li class="list-group-item list-group-item-action"> <i class=" fas fa-user-tie"> </i> Kepala sekolah </li>
+                <li class="list-group-item list-group-item-action"><i class=" fa fa-user"> </i> Porta ac consectetur ac</li>
+              </ul>   
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-12 col-md-12">
+				<div class="row">
+					<div class="col-xs-12 col-md-3 text-left" style="border-right: 2px solid #31708f">
+						<i class="fa fa-user"></i> Guru : <font class="text-info">11</font><br>
+						<i class="fa fa-user"></i>Siswa Laki-laki : <font class="text-info">106</font><br>
+						<i class="fa fa-user"></i>Siswa Perempuan : <font class="text-info">97</font><br>
+						<i class="fa fa-list-alt"></i> Rombongan Belajar : <font class="text-info">6</font><br>
+					</div>
+					<div class="col-xs-12 col-md-3 text-left" style="border-right: 2px solid #31708f">
+						<i class="fa fa-education"></i> Kurikulum : <font class="text-info">K-13</font><br>
+						<i class="fa fa-globe"></i> Penyelenggaraan : <font class="text-info">Pagi/6h</font>
+							<br>
+						<i class="fa fa-cog"></i> Manajemen Berbasis Sekolah : <i class="fa fa-uncheck text-red"></i><br>
+						<i class="fa fa-cog"></i> Semester Data : <font class="text-info">2020/2021-1</font><br>
+					</div>
+					<div class="col-xs-12 col-md-3 text-left" style="border-right: 2px solid #31708f">
+						<i class="fa fa-signal"></i> Akses Internet : <i class="fa fa-check text-info"></i><br>
+						<i class="fa fa-flash"></i> Sumber Listrik : <i class="fa fa-check text-info"></i><br>
+						<i class="fa fa-flash"></i> Daya Listrik : <font class="text-info">900</font><br>
+						<i class="fa fa-globe"></i> Luas Tanah : <font class="text-info">2,579 M²</font> <br>
+					</div>
+					<div class="col-xs-12 col-md-3 text-left">
+						<i class="fa fa-stop"></i> Ruang Kelas : <font class="text-info">6</font> <font class="text-red small">*</font><br>
+						<i class="fa fa-tint"></i> Laboratorium : <font class="text-info">1</font> <font class="text-red small">*</font><br>
+						<i class="fa fa-book"></i> Perpustakaan : <font class="text-info">1</font> <font class="text-red small">*</font><br>
+						<i class="fa fa-hand-right"></i> Sanitasi Siswa : <font class="text-info">2</font> <font class="text-red small">*</font><br>
+					</div>
+					<div class="col-xs-12 col-md-12 text-left"><font class="text-red small">*) Penghitungan hanya untuk kondisi Baik, Rusak Ringan dan Rusak Sedang</font></div>
+				</div>
+			</div>    
+
+      
+    
+    <!-- Optional JavaScript; choose one of the two! -->
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
